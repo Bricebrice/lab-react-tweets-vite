4df7b3ca5da0ed05bbaf415dc3073db186b64c97
Brice Cabioch <cabioch.brice@gmail.com>
2024-02-02 18:08:41 +0100

    last test fail was due to the @ character missing in the user.jsx... >:(

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,10 +1,12 @@
 function User(props) {
-  const name = props.name;
-  const handle = props.handle;
+  const { name, handle } = props;
+  // const name = props.name;
+  // const handle = props.handle;
+
   return (
     <span className="user">
       <span className="name">{name}</span>
-      <span className="handle">{handle}</span>
+      <span className="handle">@{handle}</span>
     </span>
   );
 }
